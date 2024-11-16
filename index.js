c3feef4f3bb8194b97a15a2a2e3019dc987720c7
AnamarijaJakoubek <anamarija.jakoubek@fer.hr>
2024-11-16 21:28:32 +0100

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth - 150;
+canvas.width = window.innerWidth - 170;
 canvas.height = window.innerHeight - 20;
 
 // Parametri igre
